0b34370fe8193eb72383f2ddc0b29410e4a07e29
skoon <scott@lazycoder.com>
2010-03-18 12:55:54 -0700

    added an any property to show if the browser supports any of the other methods at all

--- support/borderRadius/support.borderRadius.js
+++ support/borderRadius/support.borderRadius.js
@@ -4,7 +4,8 @@
         var supported = {
             moz:false,
             webkit: false,
-            css3: false
+            css3: false,
+            any: false
         };
         var primeEl = this[0]; //just need to test a single element.
         if(primeEl === undefined) return supported;
@@ -12,6 +13,7 @@
         supported.moz = (primeEl.style["MozBorderRadius"] !== undefined);
         supported.webkit = (primeEl.style["webkitBorderRadius"] !== undefined);
         supported.css3 = (primeEl.style["borderRadius"] !== undefined);
+        if(supported.css3 && supported.webkit && supported.mox) supported.any=true;
         return supported;
     }
 
